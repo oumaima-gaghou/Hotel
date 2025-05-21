406ab2c67d6ee12e4e34d580de89b39a1378048e
oumaima-gaghou <oumaimagaghou27@hotmail.com>
2025-05-21 11:44:45 +0200

    finished

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  document.getElementById("confirmation").classList.remove("hidden");
+  this.reset(); 
+});
